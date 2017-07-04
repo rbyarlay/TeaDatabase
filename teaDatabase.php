8fa137542d58e68f1a0cbef5da215abead87b536
Ryan Byarlay <rbyarlay@gmail.com>
2017-07-04 11:43:14 -0700

    Add files via upload

---
+++ teaDatabase.php
@@ -0,0 +1,18 @@
+Intro page like my website, introduction that hides once you get into things.
+
+Then load teas below that, with sorting function header.
+
+Caffinated, herbal, alphabetic, and a search bar
+
+Database will include this information:
+	name
+	image
+	scientific name
+	caffinated
+	cooking instructions
+		water temperature
+		brew time
+	common pairings
+	medicinal properties
+		stress, relaxation, etc.
+	
